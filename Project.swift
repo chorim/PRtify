1ae716560ffa00a4d036a313949d76498f0e24c9
Insu Byeon <yuki@yuki.kr>
2023-09-05 01:23:42 +0900

    Remove additional targets

--- Project.swift
+++ Project.swift
@@ -1,29 +1,10 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
-import MyPlugin
-
-/*
-                +-------------+
-                |             |
-                |     App     | Contains PRtify App target and PRtify unit-test target
-                |             |
-         +------+-------------+-------+
-         |         depends on         |
-         |                            |
- +----v-----+                   +-----v-----+
- |          |                   |           |
- |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
- |          |                   |           |
- +----------+                   +-----------+
-
- */
+import PRtifyPlugin
 
 // MARK: - Project
-
-// Local plugin loaded
-let localHelper = LocalHelper(name: "MyPlugin")
 
 // Creates our project using a helper function defined in ProjectDescriptionHelpers
 let project = Project.app(name: "PRtify",
                           platform: .iOS,
-                          additionalTargets: ["PRtifyKit", "PRtifyUI"])
+                          additionalTargets: [])
