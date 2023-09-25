998cda5dce68569959ae45c96821ba246e78b15a
Insu Byeon <yuki@yuki.kr>
2023-09-25 16:53:25 +0900

    add AsyncButton

---
+++ Targets/PRtify/Sources/Shared/Components/AsyncButton.swift
@@ -0,0 +1,88 @@
+//
+//  AsyncButton.swift
+//  PRtify
+//
+//  Created by Insu Byeon on 2023/09/25.
+//  Copyright © 2023 is.byeon. All rights reserved.
+//
+
+import SwiftUI
+
+extension AsyncButton {
+    enum ActionOption: CaseIterable {
+        case disableButton
+        case showProgressView
+    }
+}
+
+struct AsyncButton<Label: View>: View {
+    var action: () async -> Void
+    var actionOptions = Set(ActionOption.allCases)
+    @ViewBuilder var label: () -> Label
+
+    @State private var isDisabled = false
+    @State private var showProgressView = false
+
+    var body: some View {
+        Button(
+            action: {
+                if actionOptions.contains(.disableButton) {
+                    isDisabled = true
+                }
+
+                Task {
+                    var progressViewTask: Task<Void, Error>?
+
+                    if actionOptions.contains(.showProgressView) {
+                        progressViewTask = Task {
+                            try await Task.sleep(nanoseconds: 150_000_000)
+                            showProgressView = true
+                        }
+                    }
+
+                    await action()
+                    progressViewTask?.cancel()
+
+                    isDisabled = false
+                    showProgressView = false
+                }
+            },
+            label: {
+                ZStack {
+                    label().opacity(showProgressView ? 0 : 1)
+
+                    if showProgressView {
+                        ProgressView()
+                    }
+                }
+            }
+        )
+        .disabled(isDisabled)
+    }
+}
+
+extension AsyncButton where Label == Text {
+    init(_ label: String,
+         actionOptions: Set<ActionOption> = Set(ActionOption.allCases),
+         action: @escaping () async -> Void) {
+        self.init(action: action) {
+            Text(label)
+        }
+    }
+}
+
+extension AsyncButton where Label == Image {
+    init(systemName: String,
+         actionOptions: Set<ActionOption> = Set(ActionOption.allCases),
+         action: @escaping () async -> Void) {
+        self.init(action: action) {
+            Image(systemName: systemName)
+        }
+    }
+}
+
+#Preview {
+    AsyncButton(systemName: "hand.thumbsup.fill") {
+        try? await Task.sleep(nanoseconds: 3_000_000_000)
+    }
+}
