d0231f13fab8041cd6c536bf86b073e90d9845c2
Insu Byeon <me@byeon.is>
2023-10-10 05:04:08 +0900

    add new color

--- Targets/PRtify/Sources/Extensions/Color+PRtify.swift
+++ Targets/PRtify/Sources/Extensions/Color+PRtify.swift
@@ -11,9 +11,11 @@ import SwiftUI
 extension Color {
     static let flatDarkBackground = Color(.flatDarkBackground)
     static let flatDarkCardBackground = Color(.flatDarkCardBackground)
+    static let flatDarkContainerBackground = Color(.flatDarkContainerBackground)
 }
 
 extension UIColor {
     static let flatDarkBackground = UIColor(red: 36/255, green: 36/255, blue: 36/255, alpha: 1)
     static let flatDarkCardBackground = UIColor(red: 46/255, green: 46/255, blue: 46/255, alpha: 1)
+    static let flatDarkContainerBackground = UIColor(red: 28/255, green: 28/255, blue: 30/255, alpha: 1)
 }
