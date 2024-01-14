04af4e13a8c658d15d424d6412d531642a18a09e
Insu Byeon <yuki@yuki.kr>
2024-01-15 01:16:22 +0900

    chore: fix script errors

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -11,11 +11,13 @@ os=$(uname -a)
 echo "운영 체제 정보: $os"
 
 # CPU 정보 출력
-cpu_info=$(lscpu)
-echo "CPU 정보: $cpu_info"
+cpu_info=$(sysctl -n machdep.cpu.brand_string)
+cpus_virtual=$(sysctl -n hw.ncpu)
+
+echo "CPU 정보: $cpu_info ($cpus_virtual Core)"
 
 # 메모리 정보 출력
-memory_info=$(free -m)
+memory_info=$(sysctl -n hw.memsize)
 echo "메모리 정보: $memory_info"
 
 # 디스크 사용량 출력
--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,7 +5,7 @@ echo "Release: PRE-Xcode Build is activated .... "
 # for future reference
 # https://developer.apple.com/documentation/xcode/environment-variable-reference
 
-cd ../PRtify/
+cd ..
 
 plutil -replace GITHUB_API_KEY -string $GITHUB_API_KEY ./Targets/PRtify/Resources/iOS/PRtify.plist
 plutil -replace GITHUB_API_SECRET -string $GITHUB_API_SECRET ./Targets/PRtify/Resources/iOS/PRtify.plist
